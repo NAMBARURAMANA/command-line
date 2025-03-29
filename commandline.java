dab86704c983dc8884b09781d0009e0c49ddf137
NAMBARURAMANA <ramananambaru7592@gmail.com>
2025-03-29 12:19:36 +0530

    Add files via upload

---
+++ commandline.java
@@ -0,0 +1,19 @@
+class Command_line
+{
+public static void main (String args[])
+{
+int n1=Integer.perseInt(args[0]);
+int n2=Integer.perseInt(args[1]);
+
+int r1=n1+n2;
+int r1=n1-n2;
+int r1=n1*n2;
+int r1=n1/n2;
+System.out.println("Addition:"+r1);
+System.out.println("substraction:"+r2);
+System.out.println("multiplication:"+r3);
+System.out.println("division:"+r4);
+}
+}
+
+
